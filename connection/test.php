0ddfbbe74cb784a3e9090fede4a7868bab9ac1d1
root <root@LAPTOP-SP9SEKFQ.localdomain>
2020-12-11 11:49:57 +0800

    MyWeb-1.0.1

---
+++ connection/test.php
@@ -0,0 +1,13 @@
+﻿<?php
+function test($a) {
+	switch (1) {
+		case $a == '1':
+		case strlen($a) > 3:
+		case $a == '0':
+			return 0;
+		default:
+			return 1;
+	}
+}
+print_r(test('2222'));
+?>
--- connection/user-login.php
+++ connection/user-login.php
@@ -1,8 +1,27 @@
 <?php
+function check_user($username, $passwd) {
+	switch (1) {
+		case strlen($username) > 18:
+		case strlen($passwd) < 5:
+		case strlen($passwd) > 18:
+			return 0;
+		default:
+			return 1;
+	}
+}
+
 function user_login ($username, $passwd) {
+
+	if(!check_user($username, $passwd)) {
+		$ret["{replace-title}"] = "WARNING";
+		$ret["{replace-message}"] = "What're you doing now??!";
+		return $ret;
+	}
+
 	include_once ("./connection/sql-connect.php");
 
-	$sql = "SELECT * FROM users WHERE username='$username' and passwd='$passwd'";
+	$passwd_md5 = md5($passwd);
+	$sql = "SELECT * FROM users WHERE username='$username' and passwd='$passwd_md5'";
 	$result = mysqli_query($con, $sql);
 	$row = mysqli_fetch_array($result);
 
@@ -11,8 +30,8 @@ function user_login ($username, $passwd) {
 		$ret["{replace-title}"] = "SUCCESS";
 		$ret["{replace-message}"] = "login success";
 	} else {
-		$ret["{replace-title}"] = "ERROR";
-		$ret["{replace-message}"] = "login fail";
+		$ret["{replace-title}"] = "WRONG";
+		$ret["{replace-message}"] = "password wrong";
 	}
 	return $ret;
 }
